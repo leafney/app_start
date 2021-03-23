0fd4bc0cfd12672825238661f51af147f5404ae9
leafney <babycoolzx@126.com>
2021-03-23 17:35:44 +0800

    add getx

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,9 @@
 
 import 'package:app_start/page/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/screenutil_init.dart';
+import 'package:get/get.dart';
+import 'package:oktoast/oktoast.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,12 +19,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'App Start',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ScreenUtilInit(
+      designSize: Size(375, 667),
+      builder: () => OKToast(
+        // Initialize the default configuration
+        backgroundColor: Colors.black45,
+        textPadding:
+            const EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+        radius: 4.0,
+        position: ToastPosition.center,
+        child: GetMaterialApp(
+          title: 'Flutter Demo',
+          // theme: appThemeData,
+          // getPages: AppPages.pages,
+          home: HomePage(),
+          // home: InitPage(),
+        ),
       ),
-      home: HomePage(),
     );
   }
 }
